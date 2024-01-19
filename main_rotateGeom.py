193937059145768bc34485db44e53b6a9600b9d6
Zulkee <zulkeefal4@gmail.com>
2024-01-19 18:54:09 +0100

    Improved the script and used for urban example
    - Working quite well without mesh rotation and interpolation
    - Need to work further to improve performance with mesh rotation and interpolation

--- main_rotateGeom.py
+++ main_rotateGeom.py
@@ -119,24 +119,30 @@ def plotTwoFields(field1,coords1,field2,coords2):
     Plots contours for two fields on same or different meshes
     This functions is specifically made for rectangular case as it uses it boundaries and scales
     '''
+    # finding the max value of the field
+    val_max = max(np.max(field1),np.max(field2))
+    # val_min = min(np.min(field1),np.min(field2))
+    val_min = 0
+    step = 0.1
+    print ('val_max: ', val_max,' and val_min:',val_min)
     # plotting and comparing the interpolated array
     fig, (ax1,ax2) = plt.subplots(ncols=2)  
-    levels = np.arange(0.0, 5.6, .1)
+    levels = np.arange(val_min, val_max, step)
     # ax1.tricontour(coords1[:,0], coords1[:,1], field1[:,0], levels=15, linewidths=0.1, colors='k')
     cntr1 = ax1.tricontourf(coords1[:,0], coords1[:,1], field1[:,0], levels=levels, cmap="RdBu_r")
     fig.colorbar(cntr1, ax=ax1)
-    rect = patches.Rectangle((-2, -2), 4, 4, linewidth=1, edgecolor='black', facecolor='none')
-    # Add the patch to the Axes
-    ax1.add_patch(rect)
-    ax1.set(xlim=(-50, 50), ylim=(-50, 50))
+    # # Add the patch to the Axes
+    # rect = patches.Rectangle((-2, -2), 4, 4, linewidth=1, edgecolor='black', facecolor='none')
+    # ax1.add_patch(rect)
+    # ax1.set(xlim=(-50, 50), ylim=(-50, 50))
 
     # ax2.tricontour(coords2[:,0], coords2[:,1], field2[:,0], levels=15, linewidths=0.1, colors='k')
     cntr2 = ax2.tricontourf(coords2[:,0], coords2[:,1], field2[:,0], levels=levels, cmap="RdBu_r")
     fig.colorbar(cntr2, ax=ax2)
-    rect = patches.Rectangle((-2, -2), 4, 4, linewidth=1, edgecolor='black', facecolor='none')
-    # Add the patch to the Axes
-    ax2.add_patch(rect)
-    ax2.set(xlim=(-50, 50), ylim=(-50, 50))
+    # # Add the patch to the Axes
+    # rect = patches.Rectangle((-2, -2), 4, 4, linewidth=1, edgecolor='black', facecolor='none')
+    # ax2.add_patch(rect)
+    # ax2.set(xlim=(-50, 50), ylim=(-50, 50))
     
     plt.show()
     return
@@ -159,23 +165,43 @@ def addMean(array,mean):
     aux = np.copy(array)
     array = np.add(aux,mean)
     return array
+
+def rotate_point(point, angle, center_point=(0, 0)):
+    """Rotates a point around center_point(origin by default)
+    Angle is in degrees.
+    Rotation is counter-clockwise
+    """
+    angle_rad = np.radians(angle % 360)
+    # Shift the point so that center_point becomes the origin
+    new_point = (point[0] - center_point[0], point[1] - center_point[1])
+    new_point = (new_point[0] * np.cos(angle_rad) - new_point[1] * np.sin(angle_rad),
+                 new_point[0] * np.sin(angle_rad) + new_point[1] * np.cos(angle_rad))
+    # Reverse the shifting we have done
+    new_point = (new_point[0] + center_point[0], new_point[1] + center_point[1])
+    return new_point
+
+def rotate_Mesh(coords,angle, center_point=(0, 0)):
+    for ipoin in range(coords.shape[0]):
+        point = (coords[ipoin,0],coords[ipoin,1])
+        coords[ipoin,:] = rotate_point(point,angle,center_point=(0, 0))
+    return coords
 #%% MAIN
 
 # User inputs
 field = "U"                                            # fields available: U, P
-angle_new = 60
+angle_new = 65
 use_SVD = True                                         # perform SVD or not
-subtract_mean = True                                  # subtract mean for SVD
+subtract_mean = True                                   # subtract mean for SVD
 interp_method = 'Rbf'                                  # interp_quad, lagrange,Rbf
-grados = [0,30,90,120,150,180]                         # angles available: 0,30,60,90,120,150,180
+grados = [degree_div_10*10 for degree_div_10 in list(range(36))]                         # angles available
+# grados = [degree_div_10*10 for degree_div_10 in list(range(5,9))]                         # angles available
 fillValue = 1000                                       # random high value to fill-in my mesh interpolator
 ric = 1.00                                             # information content to retain in the POD modes 
-mesh_interpolation = 'cubic'                           # interpolation to be used by griddata
-npoins = 100                                           # no of points for created Structured reference mesh
-# rotateMesh = True
+mesh_interpolation = 'linear'                           # interpolation to be used by griddata
+npoins = 1000                                           # no of points for created Structured reference mesh
+rotateMesh = True
 useRefMeshForProjection = True
-
-results_folder = 'results_rotateGeom'
+results_folder = 'results_urbanExample'
 root_data = fr"{cwd}\{results_folder}"  
 file_test = ["{}_surfaces_{}".format(field, str(angle_new))] 
 
@@ -184,7 +210,7 @@ for filename in glob.glob(fr'{results_folder}\*_mode_*.vtk'):
    os.remove(filename)
 for filename in glob.glob(fr'{results_folder}\*_reference_*.vtk'):
    os.remove(filename)
-for filename in glob.glob(fr'{results_folder}\*interp.vtk'):
+for filename in glob.glob(fr'{results_folder}\*_angle_*.vtk'):
    os.remove(filename)  
      
 # Copy the mesh file 0 degree and unknown angle and delete the fields
@@ -196,47 +222,77 @@ for iangle in angles_forFieldRemoval:
     file = "{}_reference_{}.vtk".format(field, angle_new)
     # f_vtk.delete_fields(root_data, file, field) 
 
-# Create the Reference Mesh
-# read a case mesh to find the boundaries of the domain for reference mesh
-files = ["{}_surfaces_{}".format(field, grado) for grado in grados]
-_, coords_ref = f_vtk.files_vtk_to_array(root_data, [f'{field}_reference_0'],getCoords=True)   # case of 0 degree
-# create the structured mesh
-x_max = max(coords_ref[:,0])
-x_min = min(coords_ref[:,0])
-y_max = max(coords_ref[:,1])
-y_min = min(coords_ref[:,1])
-radius = max(abs(x_max),abs(x_min), abs(y_max), abs(y_min))                    # setting the radius to be the max extent
-coords_refMesh = createStructuredMesh(radius,npoins,shape='circular')
-coords_ref = coords_refMesh
+if useRefMeshForProjection:
+    # Create the Reference Mesh
+    # read a case mesh to find the boundaries of the domain for reference mesh
+    _, coords_ref = f_vtk.files_vtk_to_array(root_data, [f'{field}_reference_0'],getCoords=True)   # case of 0 degree
+    # create the structured mesh
+    x_max = max(coords_ref[:,0])
+    x_min = min(coords_ref[:,0])
+    y_max = max(coords_ref[:,1])
+    y_min = min(coords_ref[:,1])
+    radius = max(abs(x_max),abs(x_min), abs(y_max), abs(y_min))                    # setting the radius to be the max extent
+    coords_refMesh = createStructuredMesh(radius,npoins,shape='circular')
+    coords_ref = coords_refMesh
+    
+    # Calculating mesh projection error for 0 or angle_new degree case
+    angle_forMeshProjectionTest = angle_new
+    field_true, coords_test = f_vtk.files_vtk_to_array(root_data, [f'{field}_surfaces_{angle_forMeshProjectionTest}'],getCoords=True)
+    # find boundary nodes to enforce noSlip after projection   
+    boundaryNodes = np.where(field_true < 1e-10)[0]
+    field_true_interpolated = projectFieldToNewMesh(coords_test, field_true, coords_ref, mesh_interpolation ,fillValue, rescale=True)
+    # plotTwoFields(field_true,coords_test,field_true_interpolated,coords_ref)
+    field_true_projected = projectFieldToNewMesh(coords_ref, field_true_interpolated, coords_test, mesh_interpolation ,fillValue, rescale=True)
+    # correct noSlip BCs voilated due to interpolation
+    field_true_projected[boundaryNodes] = 0.0
+    
+    dir_new_file = f_vtk.array_to_file_vtk(root_data, f'{field}_reference_{angle_forMeshProjectionTest}', f'{field}_angle_{angle_forMeshProjectionTest}_meshProjection',field_true_projected, field)
+    diff_fieldMeshProjected = np.subtract(field_true[:,0],field_true_projected[:,0])
+    diff_fieldMeshProjected_norm = np.linalg.norm(diff_fieldMeshProjected,2)/np.linalg.norm(field_true,2)
+    # plotTwoFields(field_true,coords_test,field_true_projected,coords_test)
+    
+    if rotateMesh:
+        # Calculating mesh rotation+projection error for angle_new degree case
+        angle_forMeshProjectionTest = angle_new
+        field_true, coords_test = f_vtk.files_vtk_to_array(root_data, [f'{field}_surfaces_{angle_forMeshProjectionTest}'],getCoords=True)
+        coords_test = rotate_Mesh(coords_test, angle_forMeshProjectionTest)
+        field_true_interpolated = projectFieldToNewMesh(coords_test, field_true, coords_ref, mesh_interpolation ,fillValue, rescale=True)
+        # plotTwoFields(field_true,coords_test,field_true_interpolated,coords_ref)
+        field_true_projected = projectFieldToNewMesh(coords_ref, field_true_interpolated, coords_test, mesh_interpolation ,fillValue, rescale=True)
+        # correct noSlip BCs voilated due to interpolation
+        field_true_projected[boundaryNodes] = 0.0
+        
+        coords_test = rotate_Mesh(coords_test, -angle_forMeshProjectionTest)
+        dir_new_file = f_vtk.array_to_file_vtk(root_data, f'{field}_reference_{angle_forMeshProjectionTest}', f'{field}_angle_{angle_forMeshProjectionTest}_meshProjection',field_true_projected, field)
+        diff_fieldMeshProjectedAndRotated = np.subtract(field_true[:,0],field_true_projected[:,0])
+        diff_fieldMeshProjectedAndRotated_norm = np.linalg.norm(diff_fieldMeshProjected,2)/np.linalg.norm(field_true,2)
+        # plotTwoFields(field_true,coords_test,field_true_projected,coords_test)
 
-# Calculating mesh projection error for 0, angle_new degree case
-angle_forMeshProjectionTest = angle_new
-field_true, coords_test = f_vtk.files_vtk_to_array(root_data, [f'{field}_surfaces_{angle_forMeshProjectionTest}'],getCoords=True)
-field_true_interpolated = projectFieldToNewMesh(coords_test, field_true, coords_ref, mesh_interpolation ,fillValue, rescale=True)
-field_true_projected = projectFieldToNewMesh(coords_ref, field_true_interpolated, coords_test, mesh_interpolation ,fillValue, rescale=True)
-dir_new_file = f_vtk.array_to_file_vtk(root_data, f'{field}_reference_{angle_forMeshProjectionTest}', f'{field}_angle_{angle_forMeshProjectionTest}_meshProjection',field_true_projected, field)
-diff_fieldMeshProjected = np.subtract(field_true[:,0],field_true_projected[:,0])
-diff_fieldMeshProjected_norm = np.linalg.norm(diff_fieldMeshProjected,2)/np.linalg.norm(field_true,2)
-
-# Project the field inside the geom only
-square_size =2
-rectangle_coords = createStructuredMesh(square_size,npoins,shape='rectangular')
-field_projectedInTheRectangle = projectFieldToNewMesh(coords_test, field_true, rectangle_coords, mesh_interpolation ,fillValue, rescale=True,correct_FillValues=False)
-# plotTwoFields(field_true,coords_test,field_projectedInTheRectangle,rectangle_coords)
+    # # Project the field inside the geom only
+    # square_size =2
+    # rectangle_coords = createStructuredMesh(square_size,npoins,shape='rectangular')
+    # field_projectedInTheRectangle = projectFieldToNewMesh(coords_test, field_true, rectangle_coords, mesh_interpolation ,fillValue, rescale=True,correct_FillValues=False)
+    # # plotTwoFields(field_true,coords_test,field_projectedInTheRectangle,rectangle_coords)
 
 # Read the training data and intepolate if needed
+files = ["{}_surfaces_{}".format(field, grado) for grado in grados]
 iarray =0
 data_training =[]
 for ifile in files:
     array, coords = f_vtk.files_vtk_to_array(root_data, [ifile],getCoords=True)
-    array_interpolated = projectFieldToNewMesh(coords, array, coords_ref, mesh_interpolation ,fillValue, rescale=True)              
-    # plotTwoFields(field_true,coords_test,array_interpolated,coords_ref)
+    array_backup = array.copy()
+    coords_backup = coords.copy()
+    if rotateMesh and grados[iarray] != 0:
+        coords = rotate_Mesh(coords, grados[iarray])
+        # plotTwoFields(array,coords_backup,array,coords)
+    if useRefMeshForProjection:
+        array = projectFieldToNewMesh(coords, array, coords_ref, mesh_interpolation ,fillValue, rescale=True)              
+        # plotTwoFields(array_backup,coords,array,coords_ref)
     if iarray == 0:
-        data_training = array_interpolated
+        data_training = array
     else:
-        data_training = np.append(data_training,array_interpolated,axis=1)
+        data_training = np.append(data_training,array,axis=1)
     iarray = iarray +1 
-
 #%% 
 
 # Perform SVD based Interpolation
@@ -264,8 +320,8 @@ if use_SVD:
     for u_column in range(u.shape[1]):
         imode = imode+1
         new_file = "{}_mode_{}".format(field, u_column+1)
-        dir_new_file = f_vtk.array_to_file_vtk(root_data,f'{field}_reference_0', new_file, u[:, u_column], field)
-    
+        # dir_new_file = f_vtk.array_to_file_vtk(root_data,f'{field}_reference_0', new_file, u[:, u_column], field)
+            
     A_coef = np.matmul(np.transpose(u[:,:dofr]), data_training)    # coefficient matrix excluding the last basis function
     
     # # Plotting coefficients    
@@ -292,30 +348,41 @@ if use_SVD:
             f = Rbf(np.radians(grados), A_coef[i, :])
         new_coef[i,0] = f(np.radians(angle_new))
     # obtenemos el nuevo campo:
-    field_new_unrotated = np.matmul(u[:,:dofr], np.array(new_coef).reshape(-1, 1)) 
+    field_new = np.matmul(u[:,:dofr], np.array(new_coef).reshape(-1, 1)) 
     if subtract_mean:
-        field_new_unrotated[:,0] = addMean(field_new_unrotated[:,0], data_training_mean)    #add mean
+        field_new[:,0] = addMean(field_new[:,0], data_training_mean)    #add mean
 
     # comparing basis projection error, this is the minimum error ROM can achieve
     field_true, coords_test = f_vtk.files_vtk_to_array(root_data, file_test,getCoords=True)
-    field_true_interpolated = projectFieldToNewMesh(coords_test, field_true, coords_ref, mesh_interpolation ,fillValue, rescale=True)
+    field_backup = field_true.copy()
+    coords_test_backup = coords_test.copy()
+    if rotateMesh:
+        coords_test = rotate_Mesh(coords_test, angle_new)
+    if useRefMeshForProjection:
+        field_true = projectFieldToNewMesh(coords_test, field_true, coords_ref, mesh_interpolation ,fillValue, rescale=True)
     if subtract_mean:
-        field_true_interpolated[:,0] = subtractMean(field_true_interpolated[:,0], data_training_mean) 
-    actual_coef = np.matmul(np.transpose(u[:,:dofr]), field_true_interpolated)   
+        field_true[:,0] = subtractMean(field_true[:,0], data_training_mean) 
+    actual_coef = np.matmul(np.transpose(u[:,:dofr]), field_true)   
     field_true_recreated = np.matmul(u[:,:dofr], actual_coef)
     if subtract_mean:
         field_true_recreated[:,0] = addMean(field_true_recreated[:,0], data_training_mean) 
-    field_true_recreated_interpolated = projectFieldToNewMesh(coords_ref, field_true_recreated, coords_test, mesh_interpolation ,fillValue, rescale=True)
-    dir_new_file = f_vtk.array_to_file_vtk(root_data,f'{field}_reference_{angle_new}' ,f'{field}_angle_{angle_new}_romRecreated',field_true_recreated_interpolated, field)
-    diff_fieldRecreated = np.subtract(field_true[:,0],field_true_recreated_interpolated[:,0])
-    diff_fieldRecreated_norm = np.linalg.norm(diff_fieldRecreated,2)/np.linalg.norm(field_true,2)
+    if useRefMeshForProjection:
+        field_true_recreated = projectFieldToNewMesh(coords_ref, field_true_recreated, coords_test, mesh_interpolation ,fillValue, rescale=True)
+        # correct noSlip BCs voilated due to interpolation
+        field_true_recreated[boundaryNodes] = 0.0
+    if rotateMesh:
+        coords_test = rotate_Mesh(coords_test, -angle_new)
+    dir_new_file = f_vtk.array_to_file_vtk(root_data,f'{field}_reference_{angle_new}' ,f'{field}_angle_{angle_new}_romRecreated',field_true_recreated, field)
+    diff_fieldRecreated = np.subtract(field_backup[:,0],field_true_recreated[:,0])
+    diff_fieldRecreated_norm = np.linalg.norm(diff_fieldRecreated,2)/np.linalg.norm(field_backup,2)
     diff_coeffRecreated = np.subtract(actual_coef[:,0],new_coef[:,0])
     diff_coeffRecreated_norm = np.linalg.norm(diff_coeffRecreated,2)/np.linalg.norm(actual_coef,2)
+    plotTwoFields(field_backup,coords_test_backup,field_true_recreated,coords_test)
 
 # Perform Direct Mesh Interpolation
 else:
     # Interpolation to find the new field
-    field_new_unrotated = np.zeros([data_training.shape[0],1])
+    field_new = np.zeros([data_training.shape[0],1])
     for i in range(data_training.shape[0]):
         if interp_method == 'interp_quad':
             # using interp, not sure which kind of interpolation is used here
@@ -326,13 +393,22 @@ else:
         elif interp_method == 'Rbf': 
             # Radial Basis Fuction
             f = Rbf(np.radians(grados), data_training[i, :])
-        field_new_unrotated[i,0] = f(np.radians(angle_new)) 
-    
-# Rotate back the field as per its angle     
-_, coords_new = f_vtk.files_vtk_to_array(root_data, [f'{field}_surfaces_{angle_new}'],getCoords=True)
-field_new = projectFieldToNewMesh(coords_ref, field_new_unrotated, coords_new, mesh_interpolation ,fillValue, rescale=True)
+        field_new[i,0] = f(np.radians(angle_new)) 
 
-# Calculating ROM error
+if useRefMeshForProjection:    
+    # Project back the field to its original mesh     
+    _, coords_new = f_vtk.files_vtk_to_array(root_data, [f'{field}_surfaces_{angle_new}'],getCoords=True)
+    coords_new = rotate_Mesh(coords_new, angle_new)
+    field_new = projectFieldToNewMesh(coords_ref, field_new, coords_new, mesh_interpolation ,fillValue, rescale=True)
+    # correct noSlip BCs voilated due to interpolation
+    field_new[boundaryNodes] = 0.0
+
+if rotateMesh:
+    # Rotate back the field as per its angle         
+    coords_new = rotate_Mesh(coords_new, -angle_new)
+    plotTwoFields(field_backup,coords_test_backup,field_new,coords_new)
+    
+# Calculating field interpolation error
 field_true, coords_test = f_vtk.files_vtk_to_array(root_data, file_test,getCoords=True)
 diff_fieldInterp = np.subtract(field_true[:,0],field_new[:,0])
 diff_fieldInterp_norm = np.linalg.norm(diff_fieldInterp,2)/np.linalg.norm(field_true,2)
